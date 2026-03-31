7942a329a9de1b74777328598fc63d423ffa16ad
Ilya Govorukhin <Govoruxin0@gmail.com>
2026-03-31 14:26:02 +0300

    Яндекс.Метрики

--- app/app.vue
+++ app/app.vue
@@ -107,6 +107,23 @@
 </template>
 
 <script setup lang="ts">
+  useHead({
+  script: [
+    {
+      innerHTML: `
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108322152', 'ym');
+
+        ym(108322152, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+      `,
+      type: 'text/javascript'
+    }
+  ]
+})
 import { ref } from 'vue'
 
 const config = useRuntimeConfig()
