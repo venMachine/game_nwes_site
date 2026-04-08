04b3592a24832c5ecb86e9115e694ce55679ba1e
Ilya Govorukhin <Govoruxin0@gmail.com>
2026-04-08 19:35:06 +0300

    Исправлен порядок маршрутов: /sitemap теперь перед /:slug

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -4,7 +4,7 @@ const articleController = require('../controllers/articleController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/search', articleController.searchArticles);
-
+router.get('/sitemap', articleController.getAllArticlesForSitemap);
 router.get('/', articleController.getAllArticles);
 router.get('/:slug', articleController.getArticleBySlug);
 router.post('/', protect, articleController.createArticle); 
@@ -12,5 +12,5 @@ router.put('/:slug', protect, articleController.updateArticle);
 router.delete('/:slug', protect, articleController.deleteArticle);
 router.post('/publish-yandex', protect, articleController.publishToYandex);
 router.post('/publish-google', protect, articleController.publishToGoogle);
-router.get('/sitemap', articleController.getAllArticlesForSitemap);
+
 module.exports = router;
